9c4b82678686737b2e80281c05dadec7516cc27d
janaleonh <jana.leonh@gmail.com>
2019-10-21 09:52:54 +0200

    Änderungen Namen und CSS
    index.css für allgemeine Änderungen am Stylesheet
    
    theme.css gelöscht

--- src/script.js
+++ src/script.js
@@ -1,14 +1,2 @@
-var rIndex,
-    table = document.getElementById("table");
-
-
-
-
-function addRow(){
-
-  for(var i = 1; i < table.rows.length; i++){
-
-}
-}
-
-}
+import stylesheet from "./index.css";
+import theme from "./theme.css";
