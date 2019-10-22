b5d6f7cd2bf980c703a1f20e697de43935527285
JasminRa <47104858+JasminRa@users.noreply.github.com>
2019-10-22 19:27:07 +0200

    notizen
    .

--- src/hinzufuegen.js
+++ src/hinzufuegen.js
@@ -1,5 +1,6 @@
 function speicherHinzuButton(){
   document.getElementById("hinzufuegen").classList.add("hidden");
   document.getElementById("uebersicht").classList.remove("hidden");
-
+  /*brauch bei dem Tastendruck noch eine Funktion die mir die einträge selbst löscht und natürlich seite wirklich hinzufügt*/
+  
 }
