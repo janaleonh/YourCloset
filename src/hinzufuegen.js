2b2a605158ba464104c5e19dd573b79bed36f84f
JasminRa <47104858+JasminRa@users.noreply.github.com>
2019-10-23 08:21:21 +0200

    noch mehr in die Mitte
    Mitte

--- src/hinzufuegen.js
+++ src/hinzufuegen.js
@@ -2,5 +2,24 @@ function speicherHinzuButton(){
   document.getElementById("hinzufuegen").classList.add("hidden");
   document.getElementById("uebersicht").classList.remove("hidden");
   /*brauch bei dem Tastendruck noch eine Funktion die mir die einträge selbst löscht und natürlich seite wirklich hinzufügt*/
-  
+  var trhtml = document.getElementById("tabellen_id").insertRow(nummer);
+  tdhtml = document.createElement("td");
+  tdhtml.innerHTML = 'content'
+  trhtml.appendChild(tdhtml);
+
+  function ad_row()
+                {
+                        var nummer = 1;
+                        var tabellen_id = "t1";
+                        var trhtml = document.getElementById( tabellen_id ).insertRow( nummer );
+                        var tdhtml1 = document.createElement( "td" );
+                        var tdhtml2 = document.createElement( "td" );
+                        var tdhtml3 = document.createElement( "td" );
+                        tdhtml1.innerHTML = ''; // content
+                        tdhtml2.innerHTML = '';
+                        tdhtml3.innerHTML = '';
+                        trhtml.appendChild( tdhtml1 );
+                        trhtml.appendChild( tdhtml2 );
+                        trhtml.appendChild( tdhtml3 );
+                }
 }
