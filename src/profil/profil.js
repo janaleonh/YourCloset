52aeeacfc840c9956dc0aacb3c59001435f9af4f
JasminRa <47104858+JasminRa@users.noreply.github.com>
2019-10-31 11:58:08 +0100

    verbinden von profil und übersicht

--- src/profil/profil.js
+++ src/profil/profil.js
@@ -6,19 +6,24 @@ import DB from "../database.js"
 
 let _app = "";
 let _db = "";
+let _id = "";
 
 class Profil {
-    constructor(app) {
+    constructor(app,id) {
       this._app = app;
       _app = this._app;
       _db = app._db;
+      _id = id;
     }
 
     onShow() {
       // Anzuzeigende HTML-Elemente ermitteln
       let section = document.querySelector("#profil").cloneNode(true);
 
-
+      _db.kleidungAuslesen(_id).then(function(doc){
+          console.log("Kleidung ausgelesen!")
+          onFinishedLoading(doc);
+      });
 
       return {
           className: "profil",
@@ -38,6 +43,17 @@ class Profil {
   get title() {
     return "Profil";
   }
+}
+
+let onFinishedLoading = (doc) => {
+    console.log("Finished loading!");
+    document.getElementById("name") = doc.data().NAME;
+    document.getElementById("kleidungsstück") = doc.data().KATEGORIE;
+    document.getElementById("farbe") = doc.data().FARBE;
+    document.getElementById("material") = doc.data().MATERIAL;
+    document.getElementById("marke") = doc.data().MARKE;
+    document.getElementById("größe") = doc.data().GROESSE;
+    document.getElementById("wäsche") = doc.data().WAESCHE;
 }
 
 export default Profil;
